dd695b984d8a86a1cf28cb2bbdffee4b0604f5e3
NieLei <niles2005@sina.com>
2019-03-01 08:42:46 +0800

    'init'

---
+++ public/work.js
@@ -0,0 +1,47 @@
+let fs = require("fs");
+let assert = require("assert");
+let {shell} = require("electron");
+
+let app = {
+  el: "#container",
+  data: {
+    files:[]
+  },
+  mounted: function() {
+    this.loadPath({
+      name:"..",
+      path:__dirname + "/../../..",//open exe file directory
+      isDir:true
+    }); 
+  },
+  methods: {
+    openDir: function(dir) {
+      shell.openItem(dir);
+    },
+    loadPath:function(file) {
+      let self = this;
+      if(file.isDir) {
+        fs.readdir(file.path,function(err,theFiles) {
+          self.files = [{
+            name:"..",
+            path:file.path + "/..",
+            isDir:file.isDir
+          }];
+          assert(err === null);
+          theFiles.forEach(function(fileName) {
+            let theFile = {
+              name:fileName,
+              path:file.path + "/" + fileName,
+              isDir:fs.lstatSync(file.path + "/" + fileName).isDirectory()
+            };
+            self.files.push(theFile)
+          });
+        });
+      } else {
+        alert("打开文件：" + file.name)
+      }
+    }
+  }
+};
+
+new Vue(app);
